44d97291cff584a10cabf67608a3f664949eae5c
Wojtek <umboj2000@gmail.com>
2023-06-06 14:13:23 +0200

    cale zadanie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
-function App() {
+const Add = () => {
+  let location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const x = parseFloat(searchParams.get('x'));
+  const y = parseFloat(searchParams.get('y'));
+  const result = isNaN(x) || isNaN(y) ? 'Invalid arguments' : x + y;
+
+  return <h1>Wynik dodawania: {result}</h1>;
+};
+
+const Subtract = () => {
+  let location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const x = parseFloat(searchParams.get('x'));
+  const y = parseFloat(searchParams.get('y'));
+  const result = isNaN(x) || isNaN(y) ? 'Invalid arguments' : x - y;
+
+  return <h1>Wynik odejmowania: {result}</h1>;
+};
+
+const Multiply = () => {
+  let location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const x = parseFloat(searchParams.get('x'));
+  const y = parseFloat(searchParams.get('y'));
+  const result = isNaN(x) || isNaN(y) ? 'Invalid arguments' : x * y;
+
+  return <h1>Wynik mnożenia: {result}</h1>;
+};
+
+const Divide = () => {
+  let location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const x = parseFloat(searchParams.get('x'));
+  const y = parseFloat(searchParams.get('y'));
+  const result = isNaN(x) || isNaN(y) ? 'Invalid arguments' : x / y;
+
+  return <h1>Wynik dzielenia: {result}</h1>;
+};
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/add" element={<Add />} />
+        <Route path="/sub" element={<Subtract />} />
+        <Route path="/mul" element={<Multiply />} />
+        <Route path="/div" element={<Divide />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
